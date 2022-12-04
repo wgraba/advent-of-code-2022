b0d65fc62e3218f8904e1d158af0932a099a9dca
Wesley Graba <wesley.graba@gmail.com>
2022-12-04 07:31:20 +0000

    Day 2 Part 2 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -10,11 +10,47 @@ enum Move {
     SCISSORS = 3,
 }
 
-fn xlat_move(player_move: &str) -> Result<Move, ()> {
-    match player_move {
-        "A" | "X" => Ok(Move::ROCK),
-        "B" | "Y" => Ok(Move::PAPER),
-        "C" | "Z" => Ok(Move::SCISSORS),
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum Strategy {
+    WIN,
+    TIE,
+    LOSE,
+}
+
+fn xlat_opponent_move(opponent_move: &str) -> Result<Move, ()> {
+    match opponent_move {
+        "A" => Ok(Move::ROCK),
+        "B" => Ok(Move::PAPER),
+        "C" => Ok(Move::SCISSORS),
+        _ => Err(()),
+    }
+}
+
+fn xlat_my_move(opponent_move: &Move, strategy: &Strategy) -> Move {
+    match strategy {
+        Strategy::WIN => {
+            match opponent_move {
+                Move::ROCK => Move::PAPER,
+                Move::PAPER => Move::SCISSORS,
+                Move::SCISSORS => Move::ROCK,
+            }
+        }
+        Strategy::TIE => opponent_move.clone(),
+        Strategy::LOSE => {
+            match opponent_move {
+                Move::ROCK => Move::SCISSORS,
+                Move::PAPER => Move::ROCK,
+                Move::SCISSORS => Move::PAPER,
+            }
+        }
+    }
+}
+
+fn xlat_strategy(strategy: &str) -> Result<Strategy, ()> {
+    match strategy {
+        "X" => Ok(Strategy::LOSE),
+        "Y" => Ok(Strategy::TIE),
+        "Z" => Ok(Strategy::WIN),
         _ => Err(()),
     }
 }
@@ -36,16 +72,17 @@ fn main() {
 
     let mut total_score = 0;
     for input in input_reader.lines() {
-        let moves_str = input.expect("Invalid input");
-        let moves: Vec<&str> = moves_str.split(' ').collect();
+        let data_str = input.expect("Invalid input");
+        let data: Vec<&str> = data_str.split(' ').collect();
 
-        let opponent_move = xlat_move(moves[0]).unwrap();
-        let my_move = xlat_move(moves[1]).unwrap();
+        let opponent_move = xlat_opponent_move(data[0]).unwrap();
+        let strategy = xlat_strategy(data[1]).unwrap();
+        let my_move = xlat_my_move(&opponent_move, &strategy);
 
         let round_score = calc_my_round_score(&opponent_move, &my_move);
         total_score += round_score;
 
-        println!("{opponent_move:#?} {my_move:#?} {round_score}")
+        println!("{opponent_move:#?} {strategy:#?} {my_move:#?} {round_score}")
     }
 
     println!("My total score is {total_score}");
