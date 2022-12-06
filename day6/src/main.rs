914df84798a71aebb1b9c21e37298b8d89678125
Wesley Graba <wesley.graba@gmail.com>
2022-12-06 06:40:46 +0000

    Day 6 Part 1 Solution

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -9,5 +9,29 @@ fn main() {
     let input_reader = BufReader::new(puzzle_input);
     
     for input in input_reader.lines() {
+        let mut buf: Vec<char> = Vec::new();
+        let mut last_idx = 0;
+        for (i, c) in input.unwrap().char_indices() {
+            buf.push(c);
+            if buf.len() > 4 {
+                buf.remove(0);
+            }
+
+            if buf.len() >= 4 {
+                let mut sorted_buf = buf.clone();
+                sorted_buf.sort_unstable();
+                sorted_buf.dedup();
+                if sorted_buf.len() >= 4 {
+                    last_idx = i + 1;
+                    break;
+                }
+            }
+        }
+
+        if last_idx > 0 {
+            println!("{last_idx}: {buf:?}");
+        } else {
+            println!("No start of packet found!");
+        }
     }
 }
