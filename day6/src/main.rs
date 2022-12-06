9326fa97bfe79ac3eff168c4a37e2ba6f291de58
Wesley Graba <wesley.graba@gmail.com>
2022-12-06 06:43:22 +0000

    Day 6 Part 2 Solution

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -8,20 +8,21 @@ fn main() {
     let puzzle_input = File::open("input").expect("Unable to open file");
     let input_reader = BufReader::new(puzzle_input);
     
+    let PACKET_LEN = 14;
     for input in input_reader.lines() {
         let mut buf: Vec<char> = Vec::new();
         let mut last_idx = 0;
         for (i, c) in input.unwrap().char_indices() {
             buf.push(c);
-            if buf.len() > 4 {
+            if buf.len() > PACKET_LEN {
                 buf.remove(0);
             }
 
-            if buf.len() >= 4 {
+            if buf.len() >= PACKET_LEN {
                 let mut sorted_buf = buf.clone();
                 sorted_buf.sort_unstable();
                 sorted_buf.dedup();
-                if sorted_buf.len() >= 4 {
+                if sorted_buf.len() >= PACKET_LEN {
                     last_idx = i + 1;
                     break;
                 }
@@ -31,7 +32,7 @@ fn main() {
         if last_idx > 0 {
             println!("{last_idx}: {buf:?}");
         } else {
-            println!("No start of packet found!");
+            println!("No start-of-message found!");
         }
     }
 }
